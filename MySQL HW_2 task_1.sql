5f716c26c0cbe63bd7bd3a64df1b57f21903fbac
IvanPentyuhin <68154775+IvanPentyuhin@users.noreply.github.com>
2021-03-15 12:53:33 +0300

    Update and rename MySQL HW_2 task_1.sql to MySQL HW_2.sql
    add task 3-4

--- MySQL HW_2 task_1.sql
+++
@@ -1,9 +0,0 @@
-/* Задание # 2.
-/*Создайте базу данных example, разместите в ней таблицу users, состоящую из двух столбцов, числового id и строкового name.
---Создадим базу данных example
-CREATE DATABASE IF NOT EXISTS example;
---Создадим таблице users и наполнение таблицы example создадим столбец id, name.
-CREATE TABLE IF NOT EXISTS users (
-id INT UNSIGNED NOT NULL,
-name VARCHARM(20),
-);
